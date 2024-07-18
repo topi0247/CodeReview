329ad5eb74f08c20956c3a73d9b43bbaebee6ad6
topi0247 <karubona.ra247@gmail.com>
2024-07-18 15:45:35 +0900

    fix:認証の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user
   helper_method :logged_in?
+  before_action :authenticate!
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -11,5 +12,9 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     current_user.present?
+  end
+
+  def authenticate!
+    redirect_to root_path unless logged_in?
   end
 end
