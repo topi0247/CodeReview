050f19b10e75a9d76b8135c3121f8c1c97a6865c
topi0247 <karubona.ra247@gmail.com>
2024-07-19 00:10:19 +0900

    feat:ファイルの中身を取得

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,5 +8,12 @@ class RepositoriesController < ApplicationController
   def show
     @repository_name = params[:id]
     @files = Github.new(session[:github_access_token]).get_files(params[:id])
+    @content = nil
+  end
+
+  def file
+    repository_name = params[:id]
+    path = params[:path]
+    @content = Github.new(session[:github_access_token]).get_content(repository_name, path)
   end
 end
