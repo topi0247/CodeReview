6da31afe504f244db48ea2dd2038d08ed927080b
topi0247 <karubona.ra247@gmail.com>
2024-07-18 14:33:31 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@
 Rails.application.routes.draw do
   get 'up' => 'rails/health#show', as: :rails_health_check
   root 'static_pages#index'
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/github', as: :github_login
 end
