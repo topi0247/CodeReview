14ca61e84348454f41635990dc75931e1eb79047
topi0247 <karubona.ra247@gmail.com>
2024-07-18 18:18:55 +0900

    feat:リポジトリのファイル一覧表示

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -4,4 +4,9 @@ class RepositoriesController < ApplicationController
   def index
     @repositories = Github.new(session[:github_access_token]).get_repositories
   end
+
+  def show
+    @repository_name = params[:id]
+    @files = Github.new(session[:github_access_token]).get_files(params[:id])
+  end
 end
