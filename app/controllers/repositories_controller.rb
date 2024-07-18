b7bfe17daad536be87dd24cc77c7f33349c54aa5
topi0247 <karubona.ra247@gmail.com>
2024-07-18 15:47:52 +0900

    fix:別ブランチの作業を取り消し

--- app/controllers/repositories_controller.rb
+++
@@ -1,5 +0,0 @@
-class RepositoriesController < ApplicationController
-  def index
-    @repositories = Github.new(session[:github_access_token]).get_repositories
-  end
-end
--- app/services/github.rb
+++
@@ -1,9 +0,0 @@
-class Github
-  def initialize(access_token)
-    @client = Octokit::Client.new(access_token: access_token)
-  end
-
-  def get_repositories
-    @client.repositories
-  end
-end
