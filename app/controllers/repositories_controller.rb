d1812bc8752dbfb4ee6da9230f1c3b1c762e1694
topi0247 <karubona.ra247@gmail.com>
2024-07-19 01:51:24 +0900

    fix:レイアウト調整

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,11 +9,31 @@ class RepositoriesController < ApplicationController
     @repository_name = params[:id]
     @files = Github.new(session[:github_access_token]).get_files(params[:id])
     @content = nil
+    @path = nil
+    @languages = nil
   end
 
   def file
     repository_name = params[:id]
-    path = params[:path]
-    @content = Github.new(session[:github_access_token]).get_content(repository_name, path)
+    @path = params[:path]
+    @languages = set_language
+    @content = Github.new(session[:github_access_token]).get_content(repository_name, @path)
+  end
+
+  private
+
+  def set_language
+    case @path.split('.').last
+    when 'rb'
+      'ruby'
+    when 'js'
+      'javascript'
+    when 'html'
+      'html'
+    when 'erb'
+      'erb'
+    when 'slim'
+      'slim'
+    end
   end
 end
