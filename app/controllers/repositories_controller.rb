f0557b2e7deef69a6d449d77781914bab50cfe26
topi0247 <karubona.ra247@gmail.com>
2024-07-18 15:57:23 +0900

    fix:rubotop

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RepositoriesController < ApplicationController
   def index
     @repositories = Github.new(session[:github_access_token]).get_repositories
--- app/servises/github.rb
+++ app/servises/github.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Github
   def initialize(access_token)
-    @client = Octokit::Client.new(access_token: access_token)
+    @client = Octokit::Client.new(access_token:)
   end
 
   def get_repositories
