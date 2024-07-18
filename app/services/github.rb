0fb7f6357359d6009976debe1b5dd503c6141bc3
topi0247 <karubona.ra247@gmail.com>
2024-07-19 03:45:40 +0900

    fix:GraphQLに置き換え

--- app/services/github.rb
+++ app/services/github.rb
@@ -1,50 +1,117 @@
 # frozen_string_literal: true
 
+require 'graphql/client'
+require 'graphql/client/http'
+
 class Github
-  def initialize(access_token)
-    @client = Octokit::Client.new(access_token:)
+  AUTH_HEADER = "Bearer #{ENV['GITHUB_ACCESS_TOKEN']}"
+  HHTP = GraphQL::Client::HTTP.new('https://api.github.com/graphql') do
+    def headers(_context)
+      { 'Authorization': AUTH_HEADER }
+    end
+  end
+
+  Schema = GraphQL::Client.load_schema(HHTP)
+  Client = GraphQL::Client.new(schema: Schema, execute: HHTP)
+
+  REPOSITORIES_QUERY = Client.parse <<~GRAPHQL
+    query($user_name: String!) {
+      user(login: $user_name) {
+        repositories(last: 100) {
+          nodes {
+            name
+          }
+        }
+      }
+    }
+  GRAPHQL
+
+  FILES_QUERY = Client.parse <<~GRAPHQL
+    query($owner: String!, $name: String!, $expression: String!) {
+      repository(owner: $owner, name: $name) {
+        object(expression: $expression) {
+          ... on Tree {
+            entries {
+              name
+              type
+            }
+          }
+        }
+      }
+    }
+  GRAPHQL
+
+  FILE_CONTENT_QUERY = Client.parse <<~GRAPHQL
+  query($owner: String!, $name: String!, $expression: String!) {
+    repository(owner: $owner, name: $name) {
+      object(expression: $expression) {
+        ... on Blob {
+          text
+        }
+      }
+    }
+  }
+  GRAPHQL
+
+  def initialize(user_name)
+    @user_name = user_name
   end
 
   def get_repositories
-    @client.repositories
+    result = Client.query(REPOSITORIES_QUERY, variables: { user_name: @user_name })
+    if result.data && result.data.user
+      result.data.user.repositories.nodes.map { |repo| repo.name }
+    else
+      handle_errors(result)
+    end
   end
 
   def get_files(repository_name)
-    set_repo_name(repository_name)
-    files = get_all_files_recursive.flatten
-
-    files.group_by { |file| File.dirname(file[:path]) }
+    all_files = []
+    ['controllers', 'models', 'views', 'services'].each do |subdir|
+      all_files.concat(fetch_files('CodeReview', "app/#{subdir}/"))
+    end
+    all_files.group_by { |file| File.dirname(file) }
   end
 
-  def get_content(repository_name, path)
-    set_repo_name(repository_name)
-    content = @client.contents(@repo_name, path: path)
-    Base64.decode64(content.content)
+  def get_file_content(repository_name, file_path)
+    result = Client.query(FILE_CONTENT_QUERY, variables: { owner: @user_name, name: repository_name, expression: "HEAD:#{file_path}" })
+    if result.data && result.data.repository && result.data.repository.object
+      result.data.repository.object.text
+    else
+      handle_errors(result)
+    end
   end
 
   private
 
-  def get_all_files_recursive(path = '')
-    contents = @client.contents(@repo_name, path: path)
+  def handle_errors(result)
+    if result.errors.any?
+      raise "GraphQL errors: #{result.errors.map(&:message).join(', ')}"
+    else
+      raise "Unknown error: #{result.to_h.inspect}"
+    end
+  end
 
-    files = []
+  def fetch_files(repo_name, path = "")
+    result = Client.query(FILES_QUERY, variables: { owner: @user_name, name: repo_name, expression: "HEAD:#{path}" })
 
-    check_files = %w[.rb .js .html .erb .slim]
-    check_dir = %w[controllers models views services helpers javascript]
+    if result.errors.any?
+      raise "GraphQL errors: #{result.errors.map(&:message).join(', ')}"
+    end
 
-    contents.each do |content|
-      if content.type == 'file' && check_dir.any? { |dir| content.path.include?(dir) } && check_files.include?(File.extname(content.path))
-      files << { path: content.path }
-      elsif content.type == 'dir'
-      files << get_all_files_recursive(content.path)
+    entries = result.data.repository.object.entries
+    full_paths = []
+
+    entries.each do |entry|
+      full_path = "#{path}#{entry.name}"
+      if entry.type == 'blob'
+        full_paths << full_path
+      elsif entry.type == 'tree'
+        full_paths.concat(fetch_files(repo_name, "#{full_path}/"))
       end
     end
 
-    files
-  end
-
-  def set_repo_name(repository_name)
-    current_user = @client.user
-    @repo_name = "#{current_user.login}/#{repository_name}"
+    full_paths
   end
 end
