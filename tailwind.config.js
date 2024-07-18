47bcc21aa4c5e63f86a2b87cd08889c109fe027c
topi0247 <karubona.ra247@gmail.com>
2024-07-18 18:16:45 +0900

    franken-ui導入

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
+const preset = require("franken-ui/shadcn-ui/preset");
+const variables = require("franken-ui/shadcn-ui/variables");
+const ui = require("franken-ui");
+const hooks = require("franken-ui/shadcn-ui/hooks");
+
+const shadcn = hooks({
+  theme: "zinc",
+});
+
 module.exports = {
   content: [
     './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
-}
+  ],
+  presets: [preset],
+  safelist: [
+    {
+      pattern: /^uk-/,
+    },
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    variables({
+      theme: "slate",
+    }),
+    ui({
+      components: {
+        accordion: {
+          hooks: shadcn.accordion,
+        },
+      },
+    }),
+  ],
+};
