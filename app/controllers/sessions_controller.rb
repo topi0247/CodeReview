0b17cb675164f4a8351a46d40d5bd90d64650e37
topi0247 <karubona.ra247@gmail.com>
2024-07-22 01:48:05 +0900

    feat:お試しからログインした時にセッション削除

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,13 +8,13 @@ class SessionsController < ApplicationController
       u.avatar_url = auth.info.image
     end
     session[:user_id] = user.id
-    session[:github_access_token] = request.env['omniauth.auth']['credentials']['token'].to_s
+    session.delete(:try_mode) if session[:try_mode]
+    session.delete(:try_datetime) if session[:try_datetime]
     redirect_to repositories_path, notice: "Signed in!"
   end
 
   def destroy
     session.delete(:user_id)
-    session.delete(:github_access_token)
     redirect_to root_path, notice: "Signed out!", status: :see_other
   end
 end
