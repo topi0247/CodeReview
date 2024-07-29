496f485fef46bc7d085167f014602e5c6deacc0e
topi0247 <karubona.ra247@gmail.com>
2024-07-30 00:24:47 +0900

    feat:レビューログのルーティングとアクション設定

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -12,21 +12,41 @@ class RepositoriesController < ApplicationController
     @files = @git_hub.get_files(params[:id])
     @content = nil
     @path = nil
+    session[:commit_oid] = nil
+    session[:file_path] = nil
+    session[:code_review] = nil
   end
 
   def file
+    session[:file_path] = nil
+    session[:code_review] = nil
     @path = params[:path]
+    file_content = @git_hub.get_file_content(@repository_name, @path)
+    commit_oid = @git_hub.get_commit_oid(@repository_name, @path)
+    session[:commit_oid] = commit_oid
+    @commit_url = "https://github.com/#{current_user.name}/CodeReview/commit/#{commit_oid}"
     # マークダウン式でコードを表示
     # NOTE: #{@git_hub.get_file_content(@repository_name, @path)}の左にインデントがあると表示が崩れる
-    file_content = @git_hub.get_file_content(@repository_name, @path)
-    @commit_url = @git_hub.get_commit_url(@repository_name, @path)
     @content = "```#{@path.split('.').last}
 #{file_content}
 "
+    pre_review_contents = ReviewLog.find_by(repository_name: @repository_name).review_contents.find_by(file_path: @path)
+    @code_review = nil
+    if pre_review_contents.present?
+      pre_commit_oid = pre_review_contents.commit_oid
+      if pre_commit_oid == commit_oid
+        @code_review = {
+          good: pre_review_contents.content['good'],
+          improve: pre_review_contents.content['improve']
+        }
+      end
+    end
   end
 
   def code_review
     @code_review = CodeReview.code_review(current_user.name, params[:path], @repository_name)
+    session[:file_path] = params[:path]
+    session[:code_review] = @code_review
   end
 
   private
---
+++ app/controllers/review_logs_controller.rb
@@ -0,0 +1,33 @@
+class ReviewLogsController < ApplicationController
+  before_action :set_repository_name, only: %i[show create]
+
+  def index
+    @review_logs = current_user.review_logs
+  end
+
+  def show
+    @review_log = current_user.review_logs.find_by(repository_name: @repository_name)
+    if @review_log.nil?
+      redirect_to review_logs_path, alert: 'Review log not found'
+    end
+  end
+
+  def create
+    file_path = session[:file_path]
+    commit_oid = session[:commit_oid]
+    content = session[:code_review]
+    review_log = current_user.review_logs.find_by_or_build(repository_name: @repository_name)
+    review_log.review_contents.build(file_path: file_path, commit_oid: commit_oid, content: content)
+    if review_log.save
+      redirect_to repository_path(@repository_name), notice: 'Review log created'
+    else
+      redirect_to repository_path(@repository_name), notice: 'Review log not created'
+    end
+  end
+
+  private
+
+  def set_repository_name
+    @repository_name = params[:repository_id]
+  end
+end
