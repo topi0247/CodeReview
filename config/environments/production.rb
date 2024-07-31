7e9732e91eec74883d6563c3250734cfd3a76970
topi0247 <karubona.ra247@gmail.com>
2024-08-01 01:45:03 +0900

    feat:アナリティクスのタグ追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,5 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  GA.tracker = "G-R9LW0HS4WD"
 end
